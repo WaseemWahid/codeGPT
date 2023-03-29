36f90b3ba37309e6ff6e6192fe51f4e67beb17e2
Waseem <wahid.waseem@gmail.com>
2023-03-28 22:25:55 -0700

    script and index changes for query selector

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@ import user from './assets/user.svg'
 
 const form = document.querySelector('form');
 
-const chatContainer = document.querySelector('#chat__container');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
